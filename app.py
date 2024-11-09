16e231e5052f04f2f111c8594f68d1a54739c00c
Febriano Bramantyo <febriano.bramantyo@ibm.com>
2024-11-09 08:15:44 +0700

    add filter on sidebar

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ df_question = conn.read(spreadsheet=spreadsheet_master, worksheet="Question")
 df_domain = conn.read(spreadsheet=spreadsheet_master, worksheet="Domain")
 df_option = conn.read(spreadsheet=spreadsheet_master, worksheet="Option")
 df_option["Grade"] = df_option["Grade"].astype(int)
-df_response_header = conn.read(
-    spreadsheet=spreadsheet_response, worksheet="mondaydotcom_response_header"
-)
-df_response_detail = conn.read(
-    spreadsheet=spreadsheet_response, worksheet="mondaydotcom_response_detail"
-)
+df_response_header = conn.read(spreadsheet=spreadsheet_response, worksheet="mondaydotcom_response_header")
+df_response_detail = conn.read(spreadsheet=spreadsheet_response, worksheet="mondaydotcom_response_detail")
+df_band = conn.read(spreadsheet=spreadsheet_master, worksheet="Band")
+df_area = conn.read(spreadsheet=spreadsheet_master, worksheet="Area")
+df_gender = conn.read(spreadsheet=spreadsheet_master, worksheet="Gender")
+df_generation = conn.read(spreadsheet=spreadsheet_master, worksheet="Generation")
+df_yos = conn.read(spreadsheet=spreadsheet_master, worksheet="YearOfService")
 
 
 class App:
@@ -35,9 +36,21 @@ class App:
     df_sales = conn.read(spreadsheet=spreadsheet_master, worksheet="Sales")
     df = sqldf("select * from df_sales")
     cl = 0
+    key_domain = "key_domain"
+    key_band = "key_band"
+    key_area = "key_area"
+    key_gender = "key_gender"
+    key_generation = "key_generation"
+    key_yos = "key_yos"
+    
     criteria = {
         "domain": [],
-        "average": False
+        "average": False,
+        "band": [],
+        "area": [],
+        "gender": [],
+        "generation": [],
+        'yos':[]
     }
     
     def __init__(self):
@@ -57,7 +70,7 @@ class App:
                     
     def show_datavis(self):    
         self.build_header()
-        selected_domain = st.session_state["selected_domain"]
+        selected_domain = st.session_state[self.key_domain]
         sqlCommand = template_sql_command
         _sales = self.df_sales
         respondercount = sqldf(sql_cmd_responderCount)
@@ -78,18 +91,45 @@ class App:
             self.df = sqldf(sqlCommand)
             self.build_charts()
             
-    def apply_criteria(self):
-        respondercount = sqldf("select count(*) as responderCount from df_response_header")
-        respondercount = respondercount['responderCount'].loc[0]
-        selected_domain = st.session_state["selected_domain"]
-        selected_avg = True if st.session_state["selected_avg"] == 'Yes' else False
+    def build_command_criteria(self):
+        result = ""
+        bands = "" if self.criteria["band"] == [] else str(self.criteria["band"]).replace("[","").replace("]","")
+        areas = "" if self.criteria["area"] == [] else str(self.criteria["area"]).replace("[","").replace("]","")
+        genders = "" if self.criteria["gender"] == [] else str(self.criteria["gender"]).replace("[","").replace("]","")
+        if bands != "":
+            result = result + f"and a.band in ({bands})\n"
+        elif areas != "":
+            result = result + f"and a.area in ({areas})\n"
+        elif genders != "":
+            result = result + f"and a.gender in ({genders})\n"
+        else:
+            result = ""
         
-        if selected_domain != []:
-            for domain in selected_domain:
+        return result
+    
+    def apply_criteria(self):
+        self.criteria["domain"] = st.session_state[self.key_domain]
+        self.criteria["average"] = True if st.session_state["selected_avg"] == 'Yes' else False
+        self.criteria["band"] = st.session_state[self.key_band]
+        self.criteria["area"] = st.session_state[self.key_area]
+        self.criteria["gender"] = st.session_state[self.key_gender]
+        self.criteria["generation"] = st.session_state[self.key_generation]
+        self.criteria["yos"] = st.session_state[self.key_yos]
+        command_criteria = self.build_command_criteria()
+        respondercount = sqldf(f"""
+                               select count(*) as responderCount 
+                               from df_response_header as a
+                               where 1=1
+                               {command_criteria}
+                               """)
+        respondercount = respondercount['responderCount'].loc[0]
+        
+        if self.criteria["domain"] != []:
+            for domain in self.criteria["domain"]:
                 domain_id = sqldf(f"select cast(id as int) as id from df_domain where name='{domain}'")['id'][0]
                 questions = sqldf(f"select cast(id as int) as id from df_question where domain={domain_id}")
                 
-                if selected_avg:
+                if self.criteria["average"]:
                     self.apply_average(domain_id, respondercount)
                 else:
                     self.apply_detail(questions, respondercount)
@@ -99,13 +139,13 @@ class App:
             
             for domain_id in domain_ids:
                 questions = sqldf(f"select cast(id as int) as id from df_question where domain={domain_id}")
-                if selected_avg:
+                if self.criteria["average"]:
                     self.apply_average(domain_id, respondercount)
                 else:
                     self.apply_detail(questions, respondercount)
                
     def apply_average(self, id_domain, respondercount):
-        print("id_domain:", id_domain)
+        command_criteria = self.build_command_criteria()
         _df = sqldf(
             f"""
             WITH T AS(            
@@ -127,6 +167,7 @@ class App:
                 left join df_option as e on e.Id = b.Option
                 where 1=1
                 and d.Id = {id_domain}
+                {command_criteria}
             ),
             T2 AS(
                 SELECT 
@@ -177,6 +218,7 @@ class App:
                 Domain - {df_temp.loc[0,'Name']}
                 """
         )
+        st.text(f"""Total Responder {respondercount}""")
 
         fig = px.bar(
             _df,
@@ -193,6 +235,7 @@ class App:
     
     def apply_detail(self, questions, respondercount):
         for i, row in questions.iterrows():
+            command_criteria = self.build_command_criteria()
             _df = sqldf(
                 f"""
                 WITH T AS(            
@@ -212,7 +255,9 @@ class App:
                     left join df_question as c on c.Id = b.Question
                     inner join df_domain as d on d.Id = c.Domain 
                     left join df_option as e on e.Id = b.Option
-                    where b.Question = {row['id']}
+                    where 1=1
+                    and b.Question = {row['id']}
+                    {command_criteria}
                 ),
                 T2 AS(
                     SELECT 
@@ -277,12 +322,32 @@ class App:
 
             fig.update_yaxes(range=[0, respondercount], dtick=2)
             st.plotly_chart(fig)    
-        
+    
+    def reset_criteria(self):
+        st.session_state[self.key_domain] = []
+        st.session_state["selected_avg"] = 'No'
+        st.session_state[self.key_band] = []
+        st.session_state[self.key_area] = []
+        st.session_state[self.key_gender] = []
+        st.session_state[self.key_generation] = []
+        st.session_state[self.key_yos] = []
+        self.apply_criteria()
+    
     def build_sidebar(self):
         with self.sitebar:
-            st.multiselect("Select Domain", df_domain['Name'], [], on_change=self.apply_criteria, key="selected_domain", )
+            st.multiselect("Select Domain", df_domain['Name'], [], on_change=self.apply_criteria, key=self.key_domain)
             st.selectbox("Average per Domain", ['Yes', 'No'], index=1, key='selected_avg', on_change=self.apply_criteria)
-            st.button("OK", on_click=self.apply_criteria, key="btnShow")
+            st.multiselect("Select Band", df_band['Band'], [], on_change=self.apply_criteria, key=self.key_band)
+            st.multiselect("Select Area", df_area['Area'], [], on_change=self.apply_criteria, key=self.key_area)
+            st.multiselect("Select Gender", df_gender['Gender'], [], on_change=self.apply_criteria, key=self.key_gender)
+            st.multiselect("Select Generation", df_generation['Generation'], [], on_change=self.apply_criteria, key=self.key_generation)
+            st.multiselect("Select YoS", df_yos['YoS'], [], on_change=self.apply_criteria, key=self.key_yos)
+            
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.button(  "OK", on_click=self.apply_criteria, key="btnShow")
+            with col2:
+                st.button("Reset", on_click=self.reset_criteria, key="btnReset")
 
     def build_charts(self):
         fig = px.bar(
